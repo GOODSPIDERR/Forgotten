4e111bb7c345507efdba36872201779014c9bcd0
Vladimir Karashchuk <43484800+OffworldDemon@users.noreply.github.com>
2021-10-11 15:33:45 -0400

    startin the dialogue box

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public int index = 0;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        index++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
